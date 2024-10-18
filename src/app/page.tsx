d5d6e72e45c7bf779474f3b78da061841b8b3937
zehan12 <zehan9211@gmail.com>
2024-10-18 02:34:00 +0100

    feat: :construction: add coming soon component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { ComingSoon } from "@/components/shared";
 
 export default function Home() {
   return (
-    <div>
-      APP
-    </div>
+    <main>
+      <ComingSoon />
+    </main>
   );
 }
