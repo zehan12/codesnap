3ea6606f70d6627f76ba30dc9b6afa1e5bf1f0d3
zehan12 <zehan9211@gmail.com>
2025-07-29 20:39:50 +0530

    feat: :rotating_light: suspend auth

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,8 +0,0 @@
-import { authOptions } from "@/lib/authOptions";
-import NextAuth from "next-auth";
-
-// App Router requires Request handler functions
-const handler = NextAuth(authOptions);
-
-export const GET = handler;
-export const POST = handler;
